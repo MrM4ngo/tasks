9e2ddda2b0316afb4ce1c4c5bdc22476c5d6ca8c
David Bui <davbui32@gmail.com>
2024-02-13 15:21:09 -0500

    Testing Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript | David Bui
-            </header>
+            <header className="App-header">UD CISC275 | David Bui</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. | Hello World
