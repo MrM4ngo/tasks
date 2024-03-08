8f0b6d09a481831ee518d9d3659c6624f304cff2
David Bui <davbui32@gmail.com>
2024-03-07 20:41:47 -0500

    fixed

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+function Doubler({ onDouble }: { onDouble: () => void }): JSX.Element {
+    return <Button onClick={onDouble}>Double</Button>;
+}
+
+function Halver({ onHalve }: { onHalve: () => void }): JSX.Element {
+    return <Button onClick={onHalve}>Halve</Button>;
+}
+
 export function DoubleHalf(): JSX.Element {
-    const [dhValue, setDhValue] = useState<number>(1);
+    const [dhValue, setDhValue] = useState<number>(10);
 
-    const doubleValue = () => {
-        setDhValue(dhValue * 2);
-    };
-
-    const halveValue = () => {
-        setDhValue(dhValue / 2);
-    };
-
+    const handleDouble = () => setDhValue((prevValue) => 2 * prevValue);
+    const handleHalve = () => setDhValue((prevValue) => 0.5 * prevValue);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Button onClick={doubleValue}>Double</Button>
-            <Button onClick={halveValue}>Halve</Button>
+            <Doubler onDouble={handleDouble}></Doubler>
+            <Halver onHalve={handleHalve}></Halver>
         </div>
     );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -6,31 +6,28 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    const [leftDie, setLeftDie] = useState<number>(d6());
-    const [rightDie, setRightDie] = useState<number>(d6());
+    const [leftDieValue, setLeftDieValue] = useState<number>(1);
+    const [rightDieValue, setRightDieValue] = useState<number>(6);
 
-    const rollLeft = () => {
-        setLeftDie(d6());
-    };
-
-    const rollRight = () => {
-        setRightDie(d6());
+    const rollDie = (
+        dieSetter: React.Dispatch<React.SetStateAction<number>>
+    ) => {
+        const newValue = d6();
+        dieSetter(newValue);
     };
 
     return (
         <div>
-            <h2>Two Dice</h2>
-            <div>
-                <Button onClick={rollLeft}>Roll Left</Button>
-                <span data-testid="left-die">{leftDie}</span>
-            </div>
-            <div>
-                <Button onClick={rollRight}>Roll Right</Button>
-                <span data-testid="right-die">{rightDie}</span>
-            </div>
-            {leftDie === rightDie && (
-                <p>You {(leftDie === 1 && "Lose") || "Win"}</p>
-            )}
+            <Button onClick={() => rollDie(setLeftDieValue)}>Roll Left</Button>
+            <span data-testid="left-die">{leftDieValue}</span>
+            <Button onClick={() => rollDie(setRightDieValue)}>
+                Roll Right
+            </Button>
+            <span data-testid="right-die">{rightDieValue}</span>
+            {leftDieValue === rightDieValue &&
+                leftDieValue === 1 &&
+                " You Lose"}
+            {leftDieValue === rightDieValue && leftDieValue !== 1 && " You Win"}
         </div>
     );
 }
